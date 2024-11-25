f2e650fb035a0bbeb503b8fed57d609e184ee551
NazarM <melnyk.pn@ucu.edu.ua>
2024-11-25 20:04:37 +0200

    fixes

--- src/main/java/ua/edu/ucu/apps/Main.java
+++ src/main/java/ua/edu/ucu/apps/Main.java
@@ -2,21 +2,33 @@ package ua.edu.ucu.apps;
 
 import ua.edu.ucu.apps.task1.Student;
 import ua.edu.ucu.apps.task2.*;
+import ua.edu.ucu.apps.task3.MyImage;
+import ua.edu.ucu.apps.task3.ProxyImage;
 
 public class Main {
     public static void main(String[] args) {
-        Client client = Client.builder()
-                    .name("Client Name")
-                    .age(100)
-                    .email("test@client.mail")
-                    .build();
-        MailCode mailCode = new BirthdayMailCode();
-        MailInfo mailInfo = new MailInfo(client, mailCode);
-        MailSender mailSender = new MailSender();
-        mailSender.sendMail(mailInfo);
-        MyImage image = new ProxyImage("example.jpg");
-        image.display();
+        sendBirthdayMail();
+
+        displayImage("example.jpg");
     }
 
+    private static void sendBirthdayMail() {
+        Client client = Client.builder()
+                .name("Client Name")
+                .age(100)
+                .email("test@client.mail")
+                .build();
 
+        MailCode mailCode = new BirthdayMailCode();
+        MailInfo mailInfo = new MailInfo(client, mailCode);
+
+        MailSender.sendMail(mailInfo);
+        System.out.println("Mail sent successfully!");
+    }
+
+    private static void displayImage(String filename) {
+        MyImage image = new ProxyImage(filename);
+        image.display();
+        System.out.println("Image displayed: " + filename);
+    }
 }
